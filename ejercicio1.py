629fd3bae229ace18c3f390d61faf83f0b7a51f1
mangararr <156193833+mangararr@users.noreply.github.com>
2024-09-16 17:41:31 +0200

    Resolucion ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,12 @@
+numero = int(input("LEER NUMERO: "))
 
+if numero % 2 == 0:
+    print(f"El número {numero} es par.")
+    print("Pares descendentes:")
+    for i in range(numero, -1, -2):
+        print(i)
+else:
+    print(f"El número {numero} es impar.")
+    print("Impares descendentes:")
+    for i in range(numero, 0, -2):
+        print(i)
